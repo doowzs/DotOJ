417bffb57d59bc49cec49723e4687c86690eee3c
Tianyun Zhang <doowzs@qq.com>
2020-07-12 22:33:22 +0800

    initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Judge1.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Judge1.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Judge1.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
