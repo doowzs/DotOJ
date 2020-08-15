0ee96bedd74c8e1e551aa645f6d4d2d7036d362c
Tianyun Zhang <doowzs@qq.com>
2020-08-15 23:34:59 +0800

    code reformat

--- ClientApp/src/api-authorization/api-authorization.module.ts
+++ ClientApp/src/api-authorization/api-authorization.module.ts
@@ -8,7 +8,7 @@ import { ApplicationPaths } from './api-authorization.constants';
 import { HttpClientModule } from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
-import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @NgModule({
   imports: [
