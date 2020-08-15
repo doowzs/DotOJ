cd3538715d58898bf62cb16b75ea303c9150f315
Tianyun Zhang <doowzs@qq.com>
2020-08-16 03:23:08 +0800

    add header to admin page

--- ClientApp/src/admin/admin.module.ts
+++ ClientApp/src/admin/admin.module.ts
@@ -1,13 +1,20 @@
 ﻿import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 
 import {AuthorizeGuard} from '../api-authorization/authorize.guard';
+import {ApiAuthorizationModule} from '../api-authorization/api-authorization.module';
 
 import {AdminComponent} from './admin.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   imports: [
+    CommonModule,
     RouterModule.forChild([
       {
         path: 'admin',
@@ -21,6 +28,11 @@ import {DashboardComponent} from './dashboard/dashboard.component';
         ]
       }
     ]),
+    ApiAuthorizationModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
   ],
   declarations: [
     AdminComponent,
