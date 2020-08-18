db015371a042e815897cbb460f95a286506c60d9
Tianyun Zhang <doowzs@qq.com>
2020-08-18 20:10:22 +0800

    WIP on assignment component

--- ClientApp/src/admin/assignment/create/create.component.ts
+++ ClientApp/src/admin/assignment/create/create.component.ts
@@ -1,9 +1,23 @@
-﻿import {Component} from '@angular/core';
+﻿import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-admin-assignment-creator',
   templateUrl: './create.component.html'
 })
+// TODO: change name to assignment editor
 export class AssignmentCreatorComponent {
+  @Input() public assignmentId: number | null;
 
+  constructor() {
+  }
+
+  public submitForm() {
+    // TODO: implement this action
+  }
+
+  public createAssignment() {
+  }
+
+  public updateAssignment() {
+  }
 }
