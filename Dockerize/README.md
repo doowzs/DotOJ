22c0a039382d8241f277f9ef55c9f1d9cb4d06ea
Tianyun Zhang <doowzs@qq.com>
2020-08-19 17:41:59 +0800

    add Dockerize files and release script

---
+++ Dockerize/README.md
@@ -0,0 +1,9 @@
+# JudgeD
+
+Docker files for running Judge0 service.
+
+## References
+
+Some part of files in this repository has referred to the below project(s):
+
+- [Laradock](https://github.com/laradock/laradock)
---
+++ release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+cp ./initdb.sql ./Dockerize/mssql/initdb.sql
+
+cp ./appsettings.json ./Dockerize/web/appsettings.json
+set -i 's/localhost\\Express/mssql/g' ./Dockerize/web/appsettings.json
+
+cd Dockerize
+zip Judge1.zip -r ./*
