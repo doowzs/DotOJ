ef5c87f6c3128685bcbbe81d08f050d272d737a3
Tianyun Zhang <doowzs@qq.com>
2020-08-25 20:56:27 +0800

    add problems to sidebar of contest page

--- ClientApp/src/app/components/contest/view/view.component.ts
+++ ClientApp/src/app/components/contest/view/view.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { ContestViewDto } from '../../../interfaces/contest.interfaces';
+import { ContestService } from '../../../services/contest.service';
 
 @Component({
   selector: 'app-contest-view',
   templateUrl: './view.component.html',
   styleUrls: ['./view.component.css']
 })
-export class ContestViewComponent {
+export class ContestViewComponent implements OnInit {
   public contestId: number;
+  public contest: ContestViewDto;
 
   constructor(
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private service: ContestService
   ) {
     this.contestId = this.route.snapshot.params.contestId;
+  }
+
+  ngOnInit() {
+    this.service.getSingle(this.contestId)
+      .subscribe(contest => this.contest = contest);
   }
 }
