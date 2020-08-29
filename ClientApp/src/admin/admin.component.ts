c1a8b72f2d2993758c3c5cb6d58a212e6a75f504
Tianyun Zhang <doowzs@qq.com>
2020-08-29 22:00:23 +0800

    implement layout of admin pages

--- ClientApp/src/admin/admin.component.ts
+++ ClientApp/src/admin/admin.component.ts
@@ -1,14 +1,23 @@
 ﻿import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { AuthorizeService } from '../api-authorization/authorize.service';
 
 @Component({
   selector: 'app-admin-root',
-  templateUrl: './admin.component.html'
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
+  public username: Observable<string>;
+
   constructor(
     private title: Title,
+    private auth: AuthorizeService
   ) {
     this.title.setTitle('Administration');
+    this.username = this.auth.getUser().pipe(map(u => u && u.name));
   }
 }
--- ClientApp/src/admin/admin.module.ts
+++ ClientApp/src/admin/admin.module.ts
@@ -1,16 +1,26 @@
 ﻿import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AuthorizeGuard } from '../api-authorization/authorize.guard';
 import { ApiAuthorizationModule } from '../api-authorization/api-authorization.module';
 
 import { AdminComponent } from './admin.component';
 import { AdminDashboardComponent } from './components/dashboard/dashboard.component';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @NgModule({
   imports: [
     CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forChild([
       {
         path: 'admin', component: AdminComponent, canActivate: [AuthorizeGuard],
@@ -19,7 +29,13 @@ import { AdminDashboardComponent } from './components/dashboard/dashboard.compon
         ]
       }
     ]),
-    ApiAuthorizationModule
+    ApiAuthorizationModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzCardModule,
+    NzPageHeaderModule,
+    NzButtonModule,
+    NzIconModule
   ],
   declarations: [
     AdminComponent,
