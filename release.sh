4082d0f7def44a7b34fdc8c5e721be06e7a23d16
Tianyun Zhang <doowzs@qq.com>
2020-09-03 01:49:00 +0800

    fix continuous integration

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-cp ./initdb.sql ./Dockerize/mssql/initdb.sql
 cp ./appsettings.json.example ./Dockerize/web/appsettings.json
 
 cd Dockerize
-zip release.zip -r ./*
+zip dockerize.zip -r ./*
+mv dockerize.zip ../
