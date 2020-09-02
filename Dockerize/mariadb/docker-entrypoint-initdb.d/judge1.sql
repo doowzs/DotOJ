2668e81d93b797c4eefb987424c5111e0b2722c6
Tianyun Zhang <doowzs@qq.com>
2020-09-03 01:52:54 +0800

    fix DB init script

--- Dockerize/mariadb/docker-entrypoint-initdb.d/judge1.sql
+++ Dockerize/mariadb/docker-entrypoint-initdb.d/judge1.sql
@@ -1,7 +1,9 @@
-CREATE USER IF NOT EXISTS ''judge1''@''%'' IDENTIFIED BY ''judge1'';
+CREATE USER IF NOT EXISTS "judge1"@"%" IDENTIFIED BY "judge1";
 CREATE DATABASE IF NOT EXISTS judge1;
-GRANT ALL PRIVILEGES ON judge1.* TO ''judge1''@''%'';
+GRANT ALL PRIVILEGES ON judge1.* TO "judge1"@"%";
 
-CREATE USER IF NOT EXISTS ''hangfire''@''%'' IDENTIFIED BY ''hangfire'';
+CREATE USER IF NOT EXISTS "hangfire"@"%" IDENTIFIED BY "hangfire";
 CREATE DATABASE IF NOT EXISTS hangfire;
-GRANT ALL PRIVILEGES ON hangfire.* TO ''hangfire''@''%'';
+GRANT ALL PRIVILEGES ON hangfire.* TO "hangfire"@"%";
+
+FLUSH PRIVILEGES;
