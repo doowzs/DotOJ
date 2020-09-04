789d429ca28c5ded1ae440c171bd1d4984bc6484
Tianyun Zhang <doowzs@qq.com>
2020-09-04 19:22:16 +0800

    update release.sh

--- release.sh
+++ release.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
+function usage() {
+  echo "Usage: ./release.sh [env|webapp|worker|package] [tag]"
+}
+
 case "$1" in
-  "env")
-    CHANGE_SOURCE=true docker build -f Dockerfile.env -t judge1-build-env .
-    ;;
-  "webapp")
-    CHANGE_SOURCE=true docker build -f Dockerfile.webapp -t ccr.ccs.tencentyun.com/doowzs/judge1-webapp:"$2" .
-    ;;
-  "worker")
-    CHANGE_SOURCE=true docker build -f Dockerfile.worker -t ccr.ccs.tencentyun.com/doowzs/judge1-worker:"$2" .
-    ;;
-  "all")
-    CHANGE_SOURCE=true docker build -f Dockerfile.env -t judge1-build-env .
-    CHANGE_SOURCE=true docker build -f Dockerfile.webapp -t ccr.ccs.tencentyun.com/doowzs/judge1-webapp:"$2" .
-    CHANGE_SOURCE=true docker build -f Dockerfile.worker -t ccr.ccs.tencentyun.com/doowzs/judge1-worker:"$2" .
-    ;;
-  "*")
-    echo "Usage: ./release.sh [env|webapp|worker|all] tag"
+"env")
+  echo "Building judge1-build-env:latest..."
+  CHANGE_SOURCE=true docker build --force-rm --no-cache -f Dockerfile.env -t judge1-build-env:latest .
+  ;;
+"webapp" | "worker")
+  if [ -z "$2" ]; then
+    usage
     exit 1
-    ;;
+  fi
+  echo "Building ccr.ccs.tencentyun.com/doowzs/judge1-$1:$2..."
+  CHANGE_SOURCE=true docker build --force-rm --no-cache -f Dockerfile.$1 -t ccr.ccs.tencentyun.com/doowzs/judge1-$1:"$2" .
+  ;;
+"*")
+  usage
+  exit 1
+  ;;
 esac
