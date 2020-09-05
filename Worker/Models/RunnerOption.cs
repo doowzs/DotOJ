02ac34b015929e102f738f169f4b9d0099e1ee06
Tianyun Zhang <doowzs@qq.com>
2020-09-05 22:14:00 +0800

    update constructor of RunnerOption, add multi-file support

--- Worker/Models/RunnerOption.cs
+++ Worker/Models/RunnerOption.cs
@@ -6,6 +6,7 @@ namespace Worker.Models
 {
     public class RunnerOptions
     {
+        [JsonIgnore] public Language Language { get; set; }
         [JsonProperty("source_code")] public string SourceCode { get; set; }
         [JsonProperty("language_id")] public int LanguageId { get; set; }
         [JsonProperty("compiler_options")] public string CompilerOptions { get; set; }
@@ -16,23 +17,30 @@ namespace Worker.Models
         [JsonProperty("wall_time_limit")] public float WallTimeLimit => CpuTimeLimit * 2.0f;
         [JsonProperty("memory_limit")] public float MemoryLimit { get; set; }
         [JsonProperty("stack_limit")] public float StackLimit => 64 * 1024.0f;
+        [JsonProperty("additional_files")] public string AdditionalFiles { get; set; }
 
-        public RunnerOptions(Submission submission, string input, string output)
+        public RunnerOptions(Problem problem, Submission submission)
         {
-            if (submission.Program is null)
-            {
-                throw new NullReferenceException("Problem of submission is not loaded.");
-            }
+            Language = submission.Program.Language.GetValueOrDefault();
+            CpuTimeLimit = problem.TimeLimit * LanguageOptions.LanguageOptionsDict[Language].TimeFactor / 1000;
+            MemoryLimit = problem.MemoryLimit;
+        }
 
-            var language = submission.Program.Language.GetValueOrDefault();
+        public RunnerOptions(Problem problem, Submission submission, string input, string output)
+            : this(problem, submission)
+        {
             SourceCode = submission.Program.Code;
-            LanguageId = LanguageOptions.LanguageOptionsDict[language].LanguageId;
-            CompilerOptions = LanguageOptions.LanguageOptionsDict[language].CompilerOptions;
+            LanguageId = LanguageOptions.LanguageOptionsDict[Language].LanguageId;
+            CompilerOptions = LanguageOptions.LanguageOptionsDict[Language].CompilerOptions;
             Stdin = input;
             ExpectedOutput = output;
-            CpuTimeLimit = submission.Problem.TimeLimit *
-                LanguageOptions.LanguageOptionsDict[language].TimeFactor / 1000;
-            MemoryLimit = submission.Problem.MemoryLimit;
+        }
+
+        public RunnerOptions(Problem problem, Submission submission, string additionalFiles)
+            : this(problem, submission)
+        {
+            LanguageId = 89; // Magic number for multi-file programs.
+            AdditionalFiles = additionalFiles;
         }
     }
 }
