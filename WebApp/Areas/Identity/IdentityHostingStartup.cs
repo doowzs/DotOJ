8d8a92c33e095d9282aef1c8d92187f1a60190eb
Tianyun Zhang <doowzs@qq.com>
2020-09-11 13:59:01 +0800

    change project name to dotOJ

--- WebApp/Areas/Identity/IdentityHostingStartup.cs
+++ WebApp/Areas/Identity/IdentityHostingStartup.cs
@@ -1,13 +1,7 @@
-using System;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 
-[assembly: HostingStartup(typeof(Judge1.Areas.Identity.IdentityHostingStartup))]
-namespace Judge1.Areas.Identity
+[assembly: HostingStartup(typeof(WebApp.Areas.Identity.IdentityHostingStartup))]
+namespace WebApp.Areas.Identity
 {
     public class IdentityHostingStartup : IHostingStartup
     {
