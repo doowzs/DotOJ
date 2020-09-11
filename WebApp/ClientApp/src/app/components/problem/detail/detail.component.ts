44124491c9ef8ae99a086887b049130b7fe3b94c
Tianyun Zhang <doowzs@qq.com>
2020-09-12 02:00:15 +0800

    add reload button to problem view page

--- WebApp/ClientApp/src/app/components/problem/detail/detail.component.ts
+++ WebApp/ClientApp/src/app/components/problem/detail/detail.component.ts
@@ -17,6 +17,7 @@ import { LanguageInfo } from '../../../consts/languages.consts';
 export class ProblemDetailComponent implements OnInit, OnDestroy {
   public user: IUser;
   public privileged = false;
+  public loading = false;
   public problemId: number;
   public problem: ProblemViewDto;
   public language: LanguageInfo;
@@ -56,10 +57,12 @@ export class ProblemDetailComponent implements OnInit, OnDestroy {
   }
 
   public loadProblem() {
+    this.loading = true;
     this.service.getSingle(this.problemId)
       .subscribe(problem => {
         this.problem = problem;
         this.title.setTitle(problem.title);
+        this.loading = false;
       });
   }
 }
