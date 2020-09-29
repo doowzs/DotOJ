28490e49907b4c2292518a2c5049579b3016fade
Tianyun Zhang <doowzs@qq.com>
2020-09-29 15:12:26 +0800

    calculate diff in local and server time, add correct time to footer

--- WebApp/ClientApp/src/app/components/footers/main/main.component.ts
+++ WebApp/ClientApp/src/app/components/footers/main/main.component.ts
@@ -1,5 +1,6 @@
 ﻿import { Component } from '@angular/core';
 import { ApplicationConfigService } from '../../../services/config.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-footer-main',
@@ -10,10 +11,15 @@ export class MainFooterComponent {
   public title: string;
   public year: string;
   public author: string;
+  public now: moment.Moment;
 
   constructor(private config: ApplicationConfigService) {
     this.title = config.title;
     this.year = new Date().getFullYear().toString();
     this.author = config.author;
+    this.now = moment().add(config.diff, 'ms');
+    setInterval(() => {
+      this.now.add(1, 's');
+    }, 1000);
   }
 }
