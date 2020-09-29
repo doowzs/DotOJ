27cbba3a1029988ab333c35254faa9b9226758a2
Tianyun Zhang <doowzs@qq.com>
2020-09-29 15:39:41 +0800

    update miscellaneous pages

--- WebApp/ClientApp/src/app/consts/verdicts.consts.ts
+++ WebApp/ClientApp/src/app/consts/verdicts.consts.ts
@@ -16,46 +16,46 @@ export interface VerdictInfo {
 export const Verdicts: VerdictInfo[] = [
   {
     code: -2, name: 'Voided', showCase: false, stage: VerdictStage.ERROR,
-    color: 'gray', explain: 'This submission is voided by a newer submission or manually.'
+    color: 'secondary', explain: 'This submission is voided by a newer submission or manually.'
   },
   {
     code: -1, name: 'Service Failed', showCase: false, stage: VerdictStage.ERROR,
-    color: 'gray', explain: 'An internal error occurred in the judging service.'
+    color: 'secondary', explain: 'An internal error occurdanger in the judging service.'
   },
   {
     code: 0, name: 'Pending', showCase: false, stage: VerdictStage.RUNNING,
-    color: 'blue', explain: 'Your code is submitted and is waiting to be processed.'
+    color: 'primary', explain: 'Your code is submitted and is waiting to be processed.'
   },
   {
     code: 1, name: 'In Queue', showCase: false, stage: VerdictStage.RUNNING,
-    color: 'blue', explain: 'Your code is submitted and is waiting to be processed.'
+    color: 'primary', explain: 'Your code is submitted and is waiting to be processed.'
   },
   {
     code: 2, name: 'Running', showCase: true, stage: VerdictStage.RUNNING,
-    color: 'blue', explain: 'Your program is running on test cases by the judging service.'
+    color: 'primary', explain: 'Your program is running on test cases by the judging service.'
   },
   {
     code: 3, name: 'Accepted', showCase: false, stage: VerdictStage.ACCEPTED,
-    color: 'green', explain: 'Your program passed all test cases.'
+    color: 'success', explain: 'Your program passed all test cases.'
   },
   {
     code: 4, name: 'Wrong Answer', showCase: true, stage: VerdictStage.REJECTED,
-    color: 'red', explain: 'The output of your program does not match expected output.'
+    color: 'danger', explain: 'The output of your program does not match expected output.'
   },
   {
     code: 5, name: 'Time Limit Exceeded', showCase: true, stage: VerdictStage.REJECTED,
-    color: 'red', explain: 'Your program did not terminate before hitting the time limit.'
+    color: 'danger', explain: 'Your program did not terminate before hitting the time limit.'
   },
   {
     code: 6, name: 'Memory Limit Exceeded', showCase: true, stage: VerdictStage.REJECTED,
-    color: 'red', explain: 'Your program hit the memory limit.'
+    color: 'danger', explain: 'Your program hit the memory limit.'
   },
   {
     code: 7, name: 'Compilation Error', showCase: false, stage: VerdictStage.REJECTED,
-    color: 'red', explain: 'Your code cannot compile.'
+    color: 'danger', explain: 'Your code cannot compile.'
   },
   {
     code: 8, name: 'Runtime Error', showCase: true, stage: VerdictStage.REJECTED,
-    color: 'red', explain: 'Your program did not exit normally with exit code 0.'
+    color: 'danger', explain: 'Your program did not exit normally with exit code 0.'
   }
 ];
