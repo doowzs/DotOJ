81f0ea80d028d2bf77fe51badb41a5f25fab7ef4
Tianyun Zhang <doowzs@qq.com>
2020-10-01 13:24:11 +0800

    add version query param to changelog static file

--- WebApp/ClientApp/src/app/components/miscellaneous/changelog/changelog.component.ts
+++ WebApp/ClientApp/src/app/components/miscellaneous/changelog/changelog.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { ApplicationConfigService } from '../../../services/config.service';
 
 @Component({
   selector: 'app-miscellaneous-changelog',
   templateUrl: './changelog.component.html'
 })
 export class ChangelogComponent {
-  constructor(private title: Title) {
+  constructor(
+    private title: Title,
+    public config: ApplicationConfigService
+  ) {
     this.title.setTitle('Changelog');
   }
 }
