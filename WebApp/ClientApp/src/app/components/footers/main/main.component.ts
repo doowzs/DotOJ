5d1a67c7b925eb999544e1b7acced98b29df2e68
Tianyun Zhang <doowzs@qq.com>
2020-10-01 13:34:41 +0800

    show webapp version in footer

--- WebApp/ClientApp/src/app/components/footers/main/main.component.ts
+++ WebApp/ClientApp/src/app/components/footers/main/main.component.ts
@@ -4,6 +4,7 @@ import { takeUntil } from 'rxjs/operators';
 import * as moment from 'moment';
 
 import { ApplicationConfigService } from '../../../services/config.service';
+import { faCopyright } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer-main',
@@ -11,16 +12,20 @@ import { ApplicationConfigService } from '../../../services/config.service';
   styleUrls: ['./main.component.css']
 })
 export class MainFooterComponent implements OnDestroy {
+  faCopyright = faCopyright;
+
   public title: string;
-  public year: string;
   public author: string;
+  public version: string;
+  public year: string;
   public now: moment.Moment;
   private destroy$ = new Subject();
 
   constructor(private config: ApplicationConfigService) {
     this.title = config.title;
-    this.year = new Date().getFullYear().toString();
     this.author = config.author;
+    this.version = config.version;
+    this.year = new Date().getFullYear().toString();
     this.now = moment().add(config.diff, 'ms');
 
     interval(1000)
