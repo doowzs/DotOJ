65113f2ee8cb4a08326eb336497ec6bb53bcffbc
Tianyun Zhang <doowzs@qq.com>
2021-02-25 19:53:24 +0800

    update rabbitmq docker config files

---
+++ Dockerize/rabbitmq/encode.sh
@@ -0,0 +1,13 @@
+﻿#!/usr/bin/env bash
+#https://stackoverflow.com/questions/41306350/how-to-generate-password-hash-for-rabbitmq-management-http-api
+
+function encode_password()
+{
+    SALT=$(od -A n -t x -N 4 /dev/urandom)
+    PASS=$SALT$(echo -n $1 | xxd -ps | tr -d '\n' | tr -d ' ')
+    PASS=$(echo -n $PASS | xxd -r -p | sha256sum | head -c 128)
+    PASS=$(echo -n $SALT$PASS | xxd -r -p | base64 | tr -d '\n')
+    echo $PASS
+}
+
+encode_password $1
