89ea4e0e659f04d6292c9f2bbb7a86216891ea02
Tianyun Zhang <doowzs@qq.com>
2021-02-26 10:29:14 +0800

    update C# language version in client app

--- WebApp/ClientApp/src/consts/languages.consts.ts
+++ WebApp/ClientApp/src/consts/languages.consts.ts
@@ -25,6 +25,6 @@ export const Languages: LanguageInfo[] = [
   { code: 4, factor: 5.0, name: 'Python 3', mode: 'python', option: '' },
   { code: 5, factor: 2.0, name: 'Golang', mode: 'golang', option: '' },
   { code: 6, factor: 2.5, name: 'Rust', mode: 'rust', option: '-O' },
-  { code: 7, factor: 1.5, name: 'C# 8.0', mode: 'csharp', option: '/o+ /d:ONLINE_JUDGE' },
+  { code: 7, factor: 1.5, name: 'C# 9.0', mode: 'csharp', option: '/o+ /d:ONLINE_JUDGE' },
   { code: 8, factor: 2.5, name: 'Haskell', mode: 'haskell', option: '-v0 -O' }
 ];
