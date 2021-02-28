20951a0a5903029d0b68f06466e00a0382524316
Tianyun Zhang <doowzs@qq.com>
2021-02-28 23:33:21 +0800

    add migration files for lab problems

--- Server/Migrations/20210227110106_AddTestKitLabs.Designer.cs
+++ Server/Migrations/20210227110106_AddTestKitLabs.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Shared;
 
 namespace Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210227110106_AddTestKitLabs")]
+    partial class AddTestKitLabs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
