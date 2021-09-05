b32ddee3d875caff1f5f0d20e65e8a765acf7bb8
zyb1029 <zyb@smail.nju.edu.cn>
2021-09-06 02:16:25 +0800

    finish Score.cs

--- Shared/Models/Score.cs
+++ Shared/Models/Score.cs
@@ -3,12 +3,19 @@ using Shared.Generics;
 
 namespace Shared.Models
 {
-  public class Score{
-    public int id {get; set; }
-    [Required] public string UserId { get; set; }
-    public ApplicationUser User { get; set; }
-    public int SubmissionId {get; set; }
-    public Submission Submission {get; set; }
-    public int Score {get; set; }
-  }
+    public class Score
+    {
+
+        public int? id { get; set; }
+
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+
+        public int? SubmissionId { get; set; }
+        public Submission Submission { get; set; }
+
+        [Required] public int? Score { get; set; }
+
+        public string Comments { get; set; }
+   }
 }
