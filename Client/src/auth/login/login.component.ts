5bab3b6438ed0b35b0c4d6c0f7c863dd126a0524
zyb1029 <zyb@smail.nju.edu.cn>
2021-09-07 23:47:51 +0800

    fix suggestions for login.component.*

--- Client/src/auth/login/login.component.ts
+++ Client/src/auth/login/login.component.ts
@@ -1,8 +1,8 @@
-import { Component } from "@angular/core";
-import { Title } from "@angular/platform-browser";
-import { FormBuilder } from "@angular/forms";
-import { AuthorizeService } from "../authorize.service";
-import { ActivatedRoute, Route, Router } from "@angular/router";
+import {Component} from "@angular/core";
+import {Title} from "@angular/platform-browser";
+import {FormBuilder} from "@angular/forms";
+import {AuthorizeService} from "../authorize.service";
+import {ActivatedRoute, Route, Router} from "@angular/router";
 
 @Component({
   selector: "auth-login",
@@ -45,7 +45,8 @@ export class LoginComponent {
         }
       );
   }
+
   tips(): void {
-    alert("请联系《问题求解》课程助教191220186。");
+    alert("请联系2021级《问题求解》课程助教191220186。");
   }
 }
