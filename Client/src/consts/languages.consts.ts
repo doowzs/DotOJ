d6e4a1d756adccab3c2db2db026529aa1d6105fa
Tianyun Zhang <doowzs@qq.com>
2021-09-28 17:37:05 +0800

    keep only C for njuse edition

--- Client/src/consts/languages.consts.ts
+++ Client/src/consts/languages.consts.ts
@@ -14,14 +14,5 @@ export const Languages: LanguageInfo[] = [
     mode: 'c_cpp',
     option: '-std=c11 -static -march=native -O2 -fno-strict-aliasing -DONLINE_JUDGE'
   },
-  {
-    code: 2,
-    factor: 1.0,
-    name: 'C++ 17',
-    mode: 'c_cpp',
-    option: '-std=c++17 -static -march=native -O2 -fno-strict-aliasing -DONLINE_JUDGE'
-  },
-  { code: 3, factor: 2.0, name: 'Java 11', mode: 'java', option: '-J-Xms64m -J-Xmx512m' },
-  { code: 4, factor: 5.0, name: 'Python 3', mode: 'python', option: '' },
   { code: 9, factor: 0.0, name: 'Archive', mode: null, option: '' }
 ];
