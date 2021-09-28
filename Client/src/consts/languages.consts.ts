8a22453b68449960647409a3356c3bcd8bf54b3a
Tianyun Zhang <doowzs@qq.com>
2021-09-28 17:34:20 +0800

    remove extra languages in njucs edition

--- Client/src/consts/languages.consts.ts
+++ Client/src/consts/languages.consts.ts
@@ -23,9 +23,5 @@ export const Languages: LanguageInfo[] = [
   },
   { code: 3, factor: 2.0, name: 'Java 11', mode: 'java', option: '-J-Xms64m -J-Xmx512m' },
   { code: 4, factor: 5.0, name: 'Python 3', mode: 'python', option: '' },
-  { code: 5, factor: 2.0, name: 'Golang', mode: 'golang', option: '' },
-  { code: 6, factor: 2.5, name: 'Rust', mode: 'rust', option: '-O' },
-  { code: 7, factor: 1.5, name: 'C# 9.0', mode: 'csharp', option: '/o+ /d:ONLINE_JUDGE' },
-  { code: 8, factor: 2.5, name: 'Haskell', mode: 'haskell', option: '-v0 -O' },
   { code: 9, factor: 0.0, name: 'Archive', mode: null, option: '' }
 ];
